7a5c044c3bf2cb50190a4204986897e77e468e3b
FTUBE <tian.yang14@imperial.ac.uk>
2016-04-15 20:32:23 +0800

    cOM

--- src/Circle.java
+++ src/Circle.java
@@ -1,11 +1,10 @@
 
 public class Circle {
 	int x,y,radius,no,capa;
-	public Circle(int _x, int _y, int _r,int _n, int _capa){
+	protected Circle(int _x, int _y, int _r,int _n, int _capa){
 		x = _x; y = _y; radius = _r;no=_n;capa = _capa;
 	}
 	public static void main(String[] args){
-		for(int i = 0; i < 1000;i++)
-			System.out.println((int)(2*Math.random())*2-1);
+		
 	}
 }
